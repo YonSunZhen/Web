7905c33feb1fd47f94fd258af386431bf8f6315b
ZhenYonSun <1315976441@qq.com>
2018-01-08 16:32:01 +0800

    添加了一个页面
    差不多了

---
+++ 添加一个页面.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class 添加一个页面 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
